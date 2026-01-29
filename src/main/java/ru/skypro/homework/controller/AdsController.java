bf9d657fabb2ac3d52e78298559750cb42b9a81b
Some Guy <someguy@testmail.site>
2026-01-29 17:47:26 +0000

    RestApiControllers added for models: User Ads Comment.

---
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -0,0 +1,116 @@
+package ru.skypro.homework.controller;
+
+import io.swagger.model.*;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * REST controller for advertisement management
+ * Provides endpoints for CRUD operations with advertisements and comments
+ */
+@RestController
+@CrossOrigin(value = "http://localhost:3000")
+public interface AdsController {
+
+    /**
+     * Add a new advertisement
+     * @param properties Advertisement properties
+     * @param image Advertisement image
+     * @return Created advertisement
+     */
+    @PostMapping("/ads")
+    ResponseEntity<Ad> addAd(@RequestPart("properties") CreateOrUpdateAd properties,
+                              @RequestPart("image") MultipartFile image);
+
+    /**
+     * Add a comment to advertisement
+     * @param id Advertisement ID
+     * @param body Comment data
+     * @return Created comment
+     */
+    @PostMapping("/ads/{id}/comments")
+    ResponseEntity<Comment> addComment(@PathVariable("id") Integer id,
+                                        @RequestBody CreateOrUpdateComment body);
+
+    /**
+     * Delete a comment
+     * @param adId Advertisement ID
+     * @param commentId Comment ID
+     * @return ResponseEntity with status
+     */
+    @DeleteMapping("/ads/{adId}/comments/{commentId}")
+    ResponseEntity<Void> deleteComment(@PathVariable("adId") Integer adId,
+                                        @PathVariable("commentId") Integer commentId);
+
+    /**
+     * Get information about advertisement
+     * @param id Advertisement ID
+     * @return Advertisement details
+     */
+    @GetMapping("/ads/{id}")
+    ResponseEntity<ExtendedAd> getAds(@PathVariable("id") Integer id);
+
+    /**
+     * Get advertisements of authenticated user
+     * @return User's advertisements
+     */
+    @GetMapping("/ads/me")
+    ResponseEntity<Ads> getAdsMe();
+
+    /**
+     * Get all advertisements
+     * @return All advertisements
+     */
+    @GetMapping("/ads")
+    ResponseEntity<Ads> getAllAds();
+
+    /**
+     * Get comments of advertisement
+     * @param id Advertisement ID
+     * @return Comments of advertisement
+     */
+    @GetMapping("/ads/{id}/comments")
+    ResponseEntity<Comments> getComments(@PathVariable("id") Integer id);
+
+    /**
+     * Delete advertisement
+     * @param id Advertisement ID
+     * @return ResponseEntity with status
+     */
+    @DeleteMapping("/ads/{id}")
+    ResponseEntity<Void> removeAd(@PathVariable("id") Integer id);
+
+    /**
+     * Update advertisement information
+     * @param id Advertisement ID
+     * @param body Advertisement data to update
+     * @return Updated advertisement
+     */
+    @PatchMapping("/ads/{id}")
+    ResponseEntity<Ad> updateAds(@PathVariable("id") Integer id,
+                                  @RequestBody CreateOrUpdateAd body);
+
+    /**
+     * Update comment
+     * @param adId Advertisement ID
+     * @param commentId Comment ID
+     * @param body Comment data to update
+     * @return Updated comment
+     */
+    @PatchMapping("/ads/{adId}/comments/{commentId}")
+    ResponseEntity<Comment> updateComment(@PathVariable("adId") Integer adId,
+                                           @PathVariable("commentId") Integer commentId,
+                                           @RequestBody CreateOrUpdateComment body);
+
+    /**
+     * Update advertisement image
+     * @param id Advertisement ID
+     * @param image New image
+     * @return Updated image
+     */
+    @PatchMapping("/ads/{id}/image")
+    ResponseEntity<Resource> updateImage(@PathVariable("id") Integer id,
+                                          @RequestPart("image") MultipartFile image);
+}
---
+++ src/main/java/ru/skypro/homework/controller/UsersController.java
@@ -0,0 +1,48 @@
+package ru.skypro.homework.controller;
+
+import io.swagger.model.NewPassword;
+import io.swagger.model.UpdateUser;
+import io.swagger.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * REST controller for user management
+ * Provides endpoints for getting, updating user information and changing password
+ */
+@RestController
+@CrossOrigin(value = "http://localhost:3000")
+public interface UsersController {
+
+    /**
+     * Get information about authenticated user
+     * @return User information
+     */
+    @GetMapping("/users/me")
+    ResponseEntity<User> getUser();
+
+    /**
+     * Update password for authenticated user
+     * @param newPassword New password data
+     * @return ResponseEntity with status
+     */
+    @PostMapping("/users/set_password")
+    ResponseEntity<Void> setPassword(@RequestBody NewPassword newPassword);
+
+    /**
+     * Update information about authenticated user
+     * @param updateUser User data to update
+     * @return Updated user information
+     */
+    @PatchMapping("/users/me")
+    ResponseEntity<UpdateUser> updateUser(@RequestBody UpdateUser updateUser);
+
+    /**
+     * Update avatar for authenticated user
+     * @param image New avatar image
+     * @return ResponseEntity with status
+     */
+    @PatchMapping("/users/me/image")
+    ResponseEntity<Void> updateUserImage(@RequestPart("image") MultipartFile image);
+}
